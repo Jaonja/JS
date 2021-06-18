8667234efcc15524d96e718fd5115c63f814a6c4
Golishon0 <jaonja69@yandex.ru>
2021-06-18 18:37:00 +0300

    slider

---
+++ script.js
@@ -0,0 +1,51 @@
+
+let count = 0;
+let width;
+
+const images = document.querySelectorAll('.sliders img');
+const sliderLine = document.querySelector('.slider-line');
+const bit = document.querySelectorAll('.sliders img');
+function init() {
+    width = document.querySelector('.sliders').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+    rollSlider();
+}
+init();
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
+
+document.querySelector('.slider-next').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+    if (count === 7){
+        count = 0
+    }
+    rollSlider();
+});
+
+document.querySelector('.slider-prev').addEventListener('click', function () {
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
+    }
+    if (count > 7){
+        count = 0
+    }
+    console.log (count)
+    rollSlider();
+});
+
+document.querySelectorAll('.bit').forEach(bit => {
+    bit.addEventListener('click', function (event) {     
+   console.log (count = event.target.dataset.class) 
+     rollSlider();
+    });
+});
+
+
+
+
