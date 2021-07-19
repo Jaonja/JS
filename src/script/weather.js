9898498fbc58b2b35764a9ba467274767252070a
Golishon0 <jaonja69@yandex.ru>
2021-07-19 23:41:06 +0300

    fix

--- src/script/weather.js
+++ src/script/weather.js
@@ -1,12 +1,18 @@
-fetch ('http://api.openweathermap.org/data/2.5/weather?q=Brest&appid=6c273cbb2593e6efce1a4c12e3db2db5')
-.then(function (resp){return resp.json() })
-.then(function (data){
-    console.log (data)
-    document.querySelector('.City').innerHTML= data.name
-    document.querySelector('.temperature').innerHTML = Math.floor(data.main.temp -273) +'&deg'
-    document.querySelector('.disclaimer').textContent= data.weather[0]['description']
-    document.querySelector('.ico').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`
-})
-.catch (function(){
-    
-})
+let city = ['Брест'];
+let inputName = document.querySelector(".input")
+city = inputName.value
+console.log (inputName.value)
+let apiKey = "6c273cbb2593e6efce1a4c12e3db2db5";
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${apiKey}`)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    let cityName = document.querySelector(".City").innerHTML = data.name ;
+    let temperature = (document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp - 273) + "&deg");
+    let disclaimer = (document.querySelector(".disclaimer").textContent = data.weather[0]["description"]);
+    let ico = (document.querySelector(".ico").innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]["icon"]}@2x.png">`);
+  })
+  .catch(function () {});
+
+
